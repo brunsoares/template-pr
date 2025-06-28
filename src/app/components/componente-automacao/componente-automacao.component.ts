973ebb9370b880073579f5c873a4ef3a34e29240
brunsoares <brunohenrique.soares@outlook.com>
2025-06-28 13:57:29 -0300

    - Ajuste automação

---
+++ src/app/components/componente-automacao/componente-automacao.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente-automacao',
+  standalone: true,
+  imports: [],
+  templateUrl: './componente-automacao.component.html',
+  styleUrl: './componente-automacao.component.scss'
+})
+export class ComponenteAutomacaoComponent {
+
+}
