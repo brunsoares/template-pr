08cadd256617497c569aa0bba23f5039823c9280
brunsoares <brunohenrique.soares@outlook.com>
2025-06-28 14:29:05 -0300

    - Ajuste quinzenal

---
+++ src/app/components/componente-quinzenal/componente-quinzenal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente-quinzenal',
+  standalone: true,
+  imports: [],
+  templateUrl: './componente-quinzenal.component.html',
+  styleUrl: './componente-quinzenal.component.scss'
+})
+export class ComponenteQuinzenalComponent {
+
+}
